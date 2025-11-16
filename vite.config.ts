abe99e8c6f36a980ecae168902c5e237e63cb940
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2025-11-16 14:31:10 -0800

    Removed UMD. Revised exports to fit standard expectations for libraries.

--- vite.config.ts
+++ vite.config.ts
@@ -5,27 +5,11 @@ export default defineConfig({
     lib: {
       entry: 'src/index.ts',
       name: 'sl-web-ogg',
-      fileName: (format) => `sl-web-ogg.${format}.js`
-    },
-    rollupOptions: {
-      external: [],
-      output: [
-        {
-          format: 'es',   // ES module format
-          dir: 'dist',
-          entryFileNames: '[name].es.js'
-        },{
-          format: 'cjs',  // CommonJS format
-          dir: 'dist',
-          entryFileNames: '[name].cjs.js',
-          exports: 'named'
-        },{
-          format: 'umd',  // UMD format
-          dir: 'dist',
-          entryFileNames: '[name].umd.js',
-          name: 'sl-web-ogg'
-        }
-      ]
+      formats: ['es', 'cjs'],
+      fileName: (format) =>
+        format === 'es'
+          ? 'sl-web-ogg.es.js'
+          : 'sl-web-ogg.cjs'
     },
     sourcemap: true
   }
